c324e5c7534cf6704a525a827a7ba16906d01e89
maxdarkx <max_darkoriginal@hotmail.com>
2017-10-22 19:06:33 -0500

    initial
    Proyecto inicial, deteccion y remocion de fondo iniciales

---
+++ menu.m
@@ -0,0 +1,116 @@
+function varargout = menu(varargin)
+% MENU MATLAB code for menu.fig
+%      MENU, by itself, creates a new MENU or raises the existing
+%      singleton*.
+%
+%      H = MENU returns the handle to a new MENU or the handle to
+%      the existing singleton*.
+%
+%      MENU('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in MENU.M with the given input arguments.
+%
+%      MENU('Property','Value',...) creates a new MENU or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before menu_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to menu_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help menu
+
+% Last Modified by GUIDE v2.5 15-Oct-2017 08:32:34
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @menu_OpeningFcn, ...
+                   'gui_OutputFcn',  @menu_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before menu is made visible.
+function menu_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to menu (see VARARGIN)
+
+% Choose default command line output for menu
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes menu wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = menu_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in bprocesar.
+function bprocesar_Callback(hObject, eventdata, handles)
+% hObject    handle to bprocesar (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+
+function tArchivo_Callback(hObject, eventdata, handles)
+% hObject    handle to tArchivo (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of tArchivo as text
+%        str2double(get(hObject,'String')) returns contents of tArchivo as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function tArchivo_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to tArchivo (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in bBuscar.
+function bBuscar_Callback(hObject, eventdata, handles)
+% hObject    handle to bBuscar (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+[archivo, ruta] = uigetfile({'*.jpg';'*.png';'*.tiff';'*.bmp'},'Seleccione la Imagen');
+set(handles.tArchivo,'string',strcat(ruta,archivo));
+axes(handles.aCargar);
+a=get(handles.tArchivo,'string');
+b=imread(a);
+imshow(b);
